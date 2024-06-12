63b9485d2424c3c1ded2c062dd2b3c57a64c9e3a
cperdomo12cr <cperdomo@arcetec.com.co>
2024-06-12 12:18:48 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 // import './topics/02-object-interfaces'
 // import './topics/03-functions'
 // import './topics/04-homework-types'
-import './topics/05-basic-desestructuring'
+// import './topics/05-basic-desestructuring'
+import './topics/06-function-desestructuring'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   hola mundo
---
+++ src/topics/06-function-desestructuring.ts
@@ -0,0 +1,41 @@
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Samsung Galaxy S10',
+    price: 699
+}
+
+const tablet: Product = {
+    description: 'iPad Pro',
+    price: 799
+}
+
+interface TaxtCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+// function taxCalculation( options: TaxtCalculationOptions) :[number, number] {
+// function taxCalculation( {tax, products}: TaxtCalculationOptions) :[number, number] {
+function taxCalculation( options: TaxtCalculationOptions) :[number, number] {
+    const {tax, products} = options;   
+    
+    let total = 0;
+
+    products.forEach( ({price}) => {
+        total += price;
+    });
+
+    return [total, total * tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const [total, taxTotal] = taxCalculation({products: shoppingCart, tax});
+
+console.log('Total: ', total);
+console.log('Total with tax: ', taxTotal);
